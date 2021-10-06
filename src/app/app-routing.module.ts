c8b46e28192da9d0bdaffade4eb4df26ebe5fe4b
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2021-10-06 16:13:31 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // URL path brauseris: "localhost:4200", HomeComponent sisu koos HTML-ga,
+                                            // mida näidatakse kasutajale
+                                            // CSS-ga kujundatakse
+                                            // .ts failiga lisatakse dünaamilisust
+  { path: "", component: HomeComponent },
+  // kui on brauseris "localhost:4200/ostukorv", siis näitab cart.component.html-i ja lisasid
+  { path: "ostukorv", component: CartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
